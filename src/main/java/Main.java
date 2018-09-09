2a3f16be2b07f738bd1130a2ab460f719ed3621f
supreme2302 <chuvashov.egor@mail.ru>
2018-09-10 00:29:35 +0300

    -

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,16 +11,33 @@ import java.util.Objects;
 public class Main {
     public static void main(String[] args) throws IOException {
         List<String> parseConf = ParseConf.readConf();
-        int defaultPort = 8080;
-        //todo: nonNull
-        var port = ParseConf.getPort(Objects.requireNonNull(parseConf).get(0));
-        port = port == null ? defaultPort : port;
+        Integer defaultPort = 8080;
+        Integer port;
+        try {
+            port = ParseConf.getPort(parseConf.get(0));
+            port = port == null ? defaultPort : port;
+        } catch (NullPointerException e) {
+            port = defaultPort;
+        }
 
-        int defaultAmountOfThreads = 16;
-        var amountOfThreads = ParseConf.getAmountOfThreads(parseConf.get(1));
-        amountOfThreads = amountOfThreads == null ? defaultAmountOfThreads : amountOfThreads;
 
-        var document_root =  ParseConf.getStaticDir(parseConf.get(2));
+        Integer defaultAmountOfThreads = 16;
+        Integer amountOfThreads;
+        try {
+            amountOfThreads = ParseConf.getAmountOfThreads(parseConf.get(1));
+            amountOfThreads = amountOfThreads == null ?
+                    defaultAmountOfThreads : amountOfThreads;
+        } catch (NullPointerException e) {
+            amountOfThreads = defaultAmountOfThreads;
+        }
+
+        String document_root;
+        try {
+            document_root =  ParseConf.getStaticDir(parseConf.get(2));
+        } catch (NullPointerException e) {
+            document_root = null;
+        }
+
 
         try (var serverSocket = new ServerSocket(port)) {
             var threadPool = new ThreadPool(amountOfThreads);
